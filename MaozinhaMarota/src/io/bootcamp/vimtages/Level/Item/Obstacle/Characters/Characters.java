eebd426465640d40a1e895ac391d32b9481a2206
fmgchaves <155589563+fmgchaves@users.noreply.github.com>
2024-06-04 10:30:57 +0100

    Create Characters.java

---
+++ MaozinhaMarota/src/io/bootcamp/vimtages/Level/Item/Obstacle/Characters/Characters.java
@@ -0,0 +1,8 @@
+package io.bootcamp.vimtages.Item.Obstacles.Characters;
+
+import io.bootcamp.vimtages.Item.Obstacles.Obstacle;
+
+public abstract class Characters extends Obstacle {
+    public Characters() {
+    }
+}
