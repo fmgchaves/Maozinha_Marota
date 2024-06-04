09e9b9ac6c171cb519c7f6f268240af8b0388e85
fmgchaves <155589563+fmgchaves@users.noreply.github.com>
2024-06-04 10:30:20 +0100

    Create Chair.java

---
+++ MaozinhaMarota/src/io/bootcamp/vimtages/Level/Item/Obstacle/Chairs/Chair.java
@@ -0,0 +1,8 @@
+package io.bootcamp.vimtages.Item.Obstacles.Chairs;
+
+import io.bootcamp.vimtages.Item.Obstacles.Obstacle;
+
+public abstract class Chairs extends Obstacle {
+    public Chairs() {
+    }
+}
