3f275392b48258e5c2ff104d46ef0cf204451c1b
fmgchaves <155589563+fmgchaves@users.noreply.github.com>
2024-06-06 23:17:43 +0100

    Update Home.java

--- MaozinhaMarota/src/io/bootcamp/vimtages/Level/Home.java
+++ MaozinhaMarota/src/io/bootcamp/vimtages/Level/Home.java
@@ -1,8 +1,6 @@
 package io.bootcamp.vimtages.Level;
 
 import io.bootcamp.vimtages.Hand;
-import io.bootcamp.vimtages.Item.Obstacle.Characters.Maggie;
-import io.bootcamp.vimtages.Item.Obstacle.Characters.Marge;
 import io.bootcamp.vimtages.Item.Obstacle.Tree;
 import io.bootcamp.vimtages.Position;
 
@@ -14,8 +12,8 @@ public class Home extends Level {
     
     @Override
     public void makeCenario () {
-        placeItem ( new Marge(new Position ( 7,5 )));
-        placeItem ( new Maggie(new Position ( 3,8 )));
+        placeItem ( new Tree (new Position ( 3,2 )));
+        
     }
     
     @Override
