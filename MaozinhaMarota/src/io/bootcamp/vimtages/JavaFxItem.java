67b5e343c9996390b97de70e27b0a10c59adecb9
fmgchaves <155589563+fmgchaves@users.noreply.github.com>
2024-06-06 23:25:03 +0100

    Update JavaFxItem.java

--- MaozinhaMarota/src/io/bootcamp/vimtages/JavaFxItem.java
+++ MaozinhaMarota/src/io/bootcamp/vimtages/JavaFxItem.java
@@ -2,6 +2,17 @@ package io.bootcamp.vimtages;
 
 import org.academiadecodigo.simplegraphics.pictures.Picture;
 
-public class JavaFxItem {
+// we will use this class to implement the Items' graphic logic
+public class JavaFxItem{
+    private Picture donnut;
+
+    public JavaFxItem(Picture donnut) {
+        this.donnut =  new Picture(40, 40,"resources/donut.avif" );
+        this.donnut.draw();
+    }
+
+
+
+
 
 }
