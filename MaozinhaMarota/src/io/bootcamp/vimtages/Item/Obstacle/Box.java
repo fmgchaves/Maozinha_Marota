5195d7e2fd3e87af2bed4cfbf56c6e1f12c51154
Carla <carla_fontinha@hotmail.com>
2024-06-10 00:24:28 +0100

    hasbooster and can break

--- MaozinhaMarota/src/io/bootcamp/vimtages/Item/Obstacle/Box.java
+++ MaozinhaMarota/src/io/bootcamp/vimtages/Item/Obstacle/Box.java
@@ -1,7 +1,19 @@
 package io.bootcamp.vimtages.Item.Obstacle;
 
 public class Box extends Obstacle{
+
+    private boolean canBreak;
+
     public Box (Integer x, Integer y, String path) {
         super (x, y, path);
     }
+
+    public boolean canBreak(){
+        return canBreak;
+    }
+
+    public void setCanBreak(boolean newCanBreak){
+        this.canBreak = newCanBreak;
+    }
+
 }
